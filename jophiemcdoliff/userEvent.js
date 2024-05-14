e627eb93c0e8c4c3b68d7f5c9938e1be198a0509
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-05-14 12:51:40 -0400

    Updates

---
+++ jophiemcdoliff/userEvent.js
@@ -0,0 +1,34 @@
+function sendPageViewEvent(url) {
+    const endpoint = 'https://locksmithlookup-magnus1000team.vercel.app/api/createAirtableEvent';
+    
+    const data = {
+      event: 'Page View',
+      url: url
+    };
+  
+    fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to send page view event');
+        }
+        console.log('Page view event sent successfully');
+      })
+      .catch(error => {
+        console.error('Error sending page view event:', error);
+      });
+  }
+  
+  // Function to fetch the current page URL and send the page view event
+  function sendCurrentPageViewEvent() {
+    const currentUrl = window.location.href;
+    sendPageViewEvent(currentUrl);
+  }
+  
+  // Call the sendCurrentPageViewEvent function when the page loads
+  window.addEventListener('load', sendCurrentPageViewEvent);
